4ebe07268d478ae351fb54d4c47f7decb4ba0c99
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2023-01-09 14:39:21 +0800

    Configured with the cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "hayctx",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       fixturesFolder: true
